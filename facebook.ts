41cc5bc512a9a8f03551b78d3564a25dbedc9b50
eD3ath <edeath1208@gmail.com>
2023-04-29 15:05:08 +0800

    dynamic pages

--- facebook.ts
+++ facebook.ts
@@ -1,7 +1,7 @@
 export type Bindings = {
-    FB_ACCESS_TOKEN: string
     FB_APP_SECRET: string
     FB_VERIFY_TOKEN: string
+    ENC_KEY: string
     OPENAI_ORG: string
     OPENAI_API: string
     DB: D1Database
@@ -10,19 +10,17 @@ export type Bindings = {
 class Facebook {
     broadcastEchoes: boolean
     graphApiVersion: string
-    accessToken: string
     database!: D1Database
 
 
     constructor(options: any | undefined = {}) {
-        this.accessToken = ''
         this.broadcastEchoes = options.broadcastEchoes || false
         this.graphApiVersion = options.graphApiVersion || 'v2.12'
     }
 
-    async sendRequest(body: any, endpoint: string = 'messages', method: string = 'POST') {
+    async sendRequest(accessToken: string, body: any, endpoint: string = 'messages', method: string = 'POST') {
         try {
-            const res = await fetch(`https://graph.facebook.com/${this.graphApiVersion}/me/${endpoint}?access_token=${this.accessToken}`, {
+            const res = await fetch(`https://graph.facebook.com/${this.graphApiVersion}/me/${endpoint}?access_token=${accessToken}`, {
                 method,
                 headers: {
                     'Content-Type': 'application/json'
@@ -42,26 +40,27 @@ class Facebook {
 
     handleFacebookData(data: any, cb: any) {
         data.entry.forEach((entry: any) => {
+            const pageId = entry.id
             entry.messaging.forEach((event: any) => {
                 if (event.message && event.message.is_echo && !this.broadcastEchoes) {
                     return
                 }
                 if (event.message && event.message.text) {
                     if (event.message.quick_reply) {
-                        cb('quick_reply', event)
+                        cb('quick_reply', pageId, event)
                     } else {
-                        cb('message', event)
+                        cb('message', pageId, event)
                     }
                 } else if (event.postback) {
-                    cb('postback', event)
+                    cb('postback', pageId, event)
                 } else if (event.read) {
-                    cb('read', event)
+                    cb('read', pageId, event)
                 } else if (event.delivery) {
-                    cb('delivery', event)
+                    cb('delivery', pageId, event)
                 } else if (event.account_linking) {
-                    cb('account_linking', event)
+                    cb('account_linking', pageId, event)
                 } else if (event.referral) {
-                    cb('referral', event)
+                    cb('referral', pageId, event)
                 } else {
                     console.log('Webhook received unknown event: ', event)
                 }
@@ -69,14 +68,14 @@ class Facebook {
         })
     }
 
-    sendTextMessage(recipientId: object | string, message: any, options: any = {}) {
+    sendTextMessage(accessToken: string, recipientId: object | string, message: any, options: any = {}) {
         if (message.quick_replies && message.quick_replies.length > 0) {
             message.quick_replies = this._formatQuickReplies(message.quick_replies)
         }
-        return this.sendMessage(recipientId, message, options)
+        return this.sendMessage(accessToken, recipientId, message, options)
     }
 
-    async sendMessage(recipientId: object | string, message: any, options: any = {}) {
+    async sendMessage(accessToken: string, recipientId: object | string, message: any, options: any = {}) {
         const recipient = this._createRecipient(recipientId)
         const messagingType = options && options.messagingType
         const notificationType = options && options.notificationType
@@ -96,51 +95,51 @@ class Facebook {
         if (options && options.typing) {
             const autoTimeout = (message && message.text) ? message.text.length * 10 : 1000
             const timeout = (typeof options.typing === 'number') ? options.typing : autoTimeout
-            await this.sendTypingIndicator(recipientId, timeout)
+            await this.sendTypingIndicator(accessToken, recipientId, timeout)
         }
-        return this.sendRequest(reqBody)
+        return this.sendRequest(accessToken, reqBody)
     }
 
-    sendTypingIndicator(recipientId: object | string, milliseconds: number) {
+    sendTypingIndicator(accessToken: string, recipientId: object | string, milliseconds: number) {
         const timeout = isNaN(milliseconds) ? 0 : milliseconds
         if (milliseconds > 20000) {
             milliseconds = 20000
             console.error('sendTypingIndicator: max milliseconds value is 20000 (20 seconds)')
         }
         return new Promise(async (resolve, reject) => {
-            await this.sendAction(recipientId, 'typing_on')
-            setTimeout(() => this.sendAction(recipientId, 'typing_off').then((json) => resolve(json)), timeout)
+            await this.sendAction(accessToken, recipientId, 'typing_on')
+            setTimeout(() => this.sendAction(accessToken, recipientId, 'typing_off').then((json) => resolve(json)), timeout)
         })
     }
 
-    sendProfileRequest(body: any, method = 'POST') {
-        return this.sendRequest(body, 'messenger_profile', method)
+    sendProfileRequest(accessToken: string, body: any, method = 'POST') {
+        return this.sendRequest(accessToken, body, 'messenger_profile', method)
     }
 
-    setGetStartedButton(payload = 'GET_STARTED') {
-        return this.sendProfileRequest({
+    setGetStartedButton(accessToken: string) {
+        return this.sendProfileRequest(accessToken, {
             get_started: {
-                payload
+                payload: 'GET_STARTED'
             }
         })
     }
 
-    deleteGetStartedButton() {
-        return this.sendProfileRequest({
+    deleteGetStartedButton(accessToken: string) {
+        return this.sendProfileRequest(accessToken, {
             fields: [
                 'get_started'
             ]
         }, 'DELETE')
     }
 
-    setPersistentMenu(buttons: any[], disableInput = false) {
+    setPersistentMenu(accessToken: string, buttons: any[], disableInput = false) {
         if (buttons && buttons[0] && buttons[0].locale !== undefined) {
             // Received an array of locales, send it as-is.
-            return this.sendProfileRequest({ persistent_menu: buttons })
+            return this.sendProfileRequest(accessToken, { persistent_menu: buttons })
         }
         // If it's not an array of locales, we'll assume is an array of buttons.
         const formattedButtons = this._formatButtons(buttons)
-        return this.sendProfileRequest({
+        return this.sendProfileRequest(accessToken, {
             persistent_menu: [{
                 locale: 'default',
                 composer_input_disabled: disableInput,
@@ -149,24 +148,24 @@ class Facebook {
         })
     }
 
-    deletePersistentMenu() {
-        return this.sendProfileRequest({
+    deletePersistentMenu(accessToken: string) {
+        return this.sendProfileRequest(accessToken, {
             fields: [
                 'persistent_menu'
             ]
         }, 'DELETE')
     }
 
-    sendAction(recipientId: object | string, action: any) {
+    sendAction(accessToken: string, recipientId: object | string, action: any) {
         const recipient = this._createRecipient(recipientId)
-        return this.sendRequest({
+        return this.sendRequest(accessToken, {
             recipient,
             sender_action: action
         })
     }
 
-    async getUserProfile(userId: string) {
-        const url = `https://graph.facebook.com/${this.graphApiVersion}/${userId}?fields=first_name,last_name,gender&access_token=${this.accessToken}`
+    async getUserProfile(userId: string, accessToken: string) {
+        const url = `https://graph.facebook.com/${this.graphApiVersion}/${userId}?fields=first_name,last_name,gender&access_token=${accessToken}`
         try {
             const res = await fetch(url)
             return await res.json()
--- functions/api/[[route]].ts
+++ functions/api/[[route]].ts
@@ -16,7 +16,7 @@ const fb = new Facebook()
 const guides = [
   {
     role: 'system',
-    content: 'You are a helpful assistant for Camp Cesaria that answers "Frequently Asked Questions" by their customers.'
+    content: 'You are a helpful assistant for Camp Cesaria that answers "Frequently Asked Questions" by their users.'
   }]
 
 dataModels.forEach((model) => {
@@ -30,7 +30,6 @@ app.get('/webhook', (c: Context) => {
   const query = c.req.query()
 
   if (query['hub.mode'] === 'subscribe' && query['hub.verify_token'] === c.env.FB_VERIFY_TOKEN) {
-    fb.accessToken = c.env.FB_ACCESS_TOKEN
     fb.database = c.env.DB
     return c.text(query['hub.challenge'], 200)
   } else {
@@ -39,7 +38,6 @@ app.get('/webhook', (c: Context) => {
 })
 
 app.post('/webhook', async (c: Context) => {
-  fb.accessToken = c.env.FB_ACCESS_TOKEN
   fb.database = c.env.DB
   const data = await c.req.json()
   const openai = new OpenAIApi(new Configuration({
@@ -53,13 +51,15 @@ app.post('/webhook', async (c: Context) => {
     return c.status(404)
   }
   await new Promise((resolve, reject) => {
-    fb.handleFacebookData(data, async (event: string, data: any) => {
+    fb.handleFacebookData(data, async (event: string, pageId: string, data: any) => {
+      const dbRes: any = await fb.database.prepare(`SELECT access_token FROM pages WHERE id='${pageId}'`).first()
+      if (!dbRes) return
       if (event === 'message') {
-        resolve(handleMessage(openai, data))
+        resolve(handleMessage(dbRes.access_token, openai, data))
       } else if (event === 'quick_reply') {
-        resolve(handleQuickReply(openai, data))
+        resolve(handleQuickReply(dbRes.access_token, openai, data))
       } else if (event === 'postback') {
-        resolve(handlePostback(c.env.DB, data))
+        resolve(handlePostback(dbRes.access_token, c.env.DB, data))
       } else if (event === 'read') {
         resolve('')
       } else if (event === 'delivery') {
@@ -76,29 +76,29 @@ app.post('/webhook', async (c: Context) => {
 })
 
 
-async function handleMessage(openai: OpenAIApi, payload: any) {
+async function handleMessage(accessToken: string, openai: OpenAIApi, payload: any) {
   const senderId = payload.sender.id
   const text = payload.message.text
   try {
     if (text) {
       if (text === 'set menu' && admins.includes(payload.sender.id)) {
-        await fb.setPersistentMenu([
+        await fb.setPersistentMenu(accessToken, [
           { type: 'postback', title: 'FAQs', payload: 'MENU_FAQ' },
           { type: 'postback', title: 'Talk to a person', payload: 'TALK_TO_PERSON' }
         ])
-        await fb.sendTextMessage(senderId, 'Menu has been set.', { typing: true })
+        await fb.sendTextMessage(accessToken, senderId, 'Menu has been set.', { typing: true })
       } else if (text === 'del menu' && admins.includes(payload.sender.id)) {
-        await fb.deletePersistentMenu()
-        await fb.sendTextMessage(senderId, 'Menu has been deleted.', { typing: true })
+        await fb.deletePersistentMenu(accessToken)
+        await fb.sendTextMessage(accessToken, senderId, 'Menu has been deleted.', { typing: true })
       } else if (text === 'set get started' && admins.includes(payload.sender.id)) {
-        await fb.setGetStartedButton('GET_STARTED')
-        await fb.sendTextMessage(senderId, 'Get Started button has been set.', { typing: true })
+        await fb.setGetStartedButton(accessToken)
+        await fb.sendTextMessage(accessToken, senderId, 'Get Started button has been set.', { typing: true })
       } else if (text === 'del get started' && admins.includes(payload.sender.id)) {
-        await fb.deletePersistentMenu()
-        await fb.deleteGetStartedButton()
-        await fb.sendTextMessage(senderId, 'Get Started button has been deleted.', { typing: true })
+        await fb.deletePersistentMenu(accessToken)
+        await fb.deleteGetStartedButton(accessToken)
+        await fb.sendTextMessage(accessToken, senderId, 'Get Started button has been deleted.', { typing: true })
       } else {
-        await sendChatPayload(openai, senderId, [...guides,
+        await sendChatPayload(accessToken, openai, senderId, [...guides,
         {
           role: 'user',
           content: text
@@ -112,17 +112,17 @@ async function handleMessage(openai: OpenAIApi, payload: any) {
 }
 
 
-async function handlePostback(database: D1Database, payload: any) {
+async function handlePostback(accessToken: string, database: D1Database, payload: any) {
   const senderId = payload.sender.id
   const event = payload.postback.payload
 
-  fb.sendTypingIndicator(senderId, 1000)
+  fb.sendTypingIndicator(accessToken, senderId, 1000)
 
   if (event === 'GET_STARTED' || event === 'MENU_FAQ') {
-    let user: any = await database.prepare(`SELECT * FROM customers WHERE id='${senderId}'`).first()
+    let user: any = await database.prepare(`SELECT * FROM users WHERE id='${senderId}'`).first()
     if (!user) {
-      user = await fb.getUserProfile(senderId)
-      await fb.database.prepare(`INSERT INTO customers(id, first_name, last_name, gender, talk_to_person) VALUES('${user.id}','${user.first_name}','${user.last_name}','${user.gender}', 0)`).run()
+      user = await fb.getUserProfile(accessToken, senderId)
+      await fb.database.prepare(`INSERT INTO users(id, first_name, last_name, gender, talk_to_person) VALUES('${user.id}','${user.first_name}','${user.last_name}','${user.gender}', 0)`).run()
     }
     const faq: any = { text: `Hi ${user.gender === 'male' ? 'Sir' : 'Ma\'am'} ${user.first_name}! How may I help you?\n${questions.map((question, i) => `${i + 1}. ${question}`).join('\n')}` }
     const menus: any[] = []
@@ -134,19 +134,19 @@ async function handlePostback(database: D1Database, payload: any) {
       })
     })
     faq.quick_replies = menus
-    await fb.sendMessage(senderId, faq, {
+    await fb.sendMessage(accessToken, senderId, faq, {
       typing: true
     })
   } else if (event === 'TALK_TO_PERSON') {
-    await fb.sendTypingIndicator(senderId, 2000)
-    await fb.database.prepare(`UPDATE customers SET talk_to_person=1 WHERE id='${senderId}'`).run()
-    await fb.sendTextMessage(senderId, 'We are currently busy serving customers inside the camp. We\'re humbly asking for your patience.', {
+    await fb.sendTypingIndicator(accessToken, senderId, 2000)
+    await fb.database.prepare(`UPDATE users SET talk_to_person=1 WHERE id='${senderId}'`).run()
+    await fb.sendTextMessage(accessToken, senderId, 'We are currently busy serving customers inside the camp. We\'re humbly asking for your patience.', {
       typing: true
     })
   }
 }
 
-async function handleQuickReply(openai: OpenAIApi, payload: any) {
+async function handleQuickReply(accessToken: string, openai: OpenAIApi, payload: any) {
   const senderId = payload.sender.id
   const quickReply = payload.message.quick_reply.payload
   if (quickReply.match(/QUESTION_\w+/g)) {
@@ -155,11 +155,11 @@ async function handleQuickReply(openai: OpenAIApi, payload: any) {
       role: 'user',
       content: questions[questionIndex]
     }]
-    await sendChatPayload(openai, senderId, messages)
+    await sendChatPayload(accessToken, openai, senderId, messages)
   }
 }
 
-async function sendChatPayload(openai: OpenAIApi, senderId: string, messages: any[]) {
+async function sendChatPayload(accessToken: string, openai: OpenAIApi, senderId: string, messages: any[]) {
   const reply = await openai.createChatCompletion({
     model: 'gpt-3.5-turbo',
     messages,
@@ -169,7 +169,7 @@ async function sendChatPayload(openai: OpenAIApi, senderId: string, messages: an
     presence_penalty: 0,
     max_tokens: 1024
   })
-  await fb.sendMessage(senderId, reply?.data?.choices[0].message?.content || 'I\m speechless', {
+  await fb.sendMessage(accessToken, senderId, reply?.data?.choices[0].message?.content || 'I\m speechless', {
     typing: true
   })
 }
